7d8c7f0b2abea5153516b00568a74c68328e9af0
DiegoBelPe <dbeltranpereira@gmail.com>
2022-06-17 21:22:42 -0500

    form paciente ok

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,7 +7,7 @@ const Formulario = () => {
       <p className='text-center mt-5 font-bold mb-5'>Añade pacientes y <span className='text-blue-700'>Administralos</span></p>
 
       <form action="" className='bg-white shadow-md rounded-lg py-10 px-5'>
-        <div>
+        <div className='mb-3'>
           <label htmlFor='mascota' className='block uppercase text-gray-700 font-bold'>Nombre de la mascota</label>
           <input 
           id='mascota'
@@ -15,6 +15,41 @@ const Formulario = () => {
           type='text' 
           placeholder='Nombre de la Mascota' />
         </div>
+        <div className='mb-3'>
+          <label htmlFor='propietario' className='block uppercase text-gray-700 font-bold'>Nombre del Propietario</label>
+          <input 
+          id='propietario'
+          className='border-2 w-full p-2 mt-2 placeholder:text-gray-500 rounded-md'
+          type='text' 
+          placeholder='Nombre del propietario' />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='email' className='block uppercase text-gray-700 font-bold'>Email</label>
+          <input 
+          id='email'
+          className='border-2 w-full p-2 mt-2 placeholder:text-gray-500 rounded-md'
+          type='email' 
+          placeholder='Email' />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='alta' className='block uppercase text-gray-700 font-bold'>Alta</label>
+          <input 
+          id='alta'
+          className='border-2 w-full p-2 mt-2 placeholder:text-gray-500 rounded-md'
+          type='date' />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='sintomas' className='block uppercase text-gray-700 font-bold'>Sintomas</label>
+          < textarea 
+          id='sintomas'
+          className='border-2 w-full p-2 mt-2 placeholder:text-gray-500 rounded-md'
+          placeholder='Descripcion de sintomas' />
+        </div>
+        <input 
+        type="submit"
+        className='bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md uppercase cursor-pointer mb-5'
+        value="Añadir paciente" />
+
       </form>
     </div>
   )
